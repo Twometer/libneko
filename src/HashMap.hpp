282dcaa728cef334a819a24a0aae9810b908eb05
Twometer <twometer@outlook.de>
2021-11-18 18:25:55 +0100

    Experimental HashMap rehashing

--- src/HashMap.hpp
+++ src/HashMap.hpp
@@ -20,11 +20,21 @@ namespace nk {
         struct Entry {
             K m_key;
             V m_value;
+            Entry *m_next = nullptr;
+            Entry *m_prev = nullptr;
         };
 
         struct Bucket {
-            nk::Vector<Entry> m_entries{};
+            Entry *m_head = nullptr;
+            Entry *m_tail = nullptr;
+            size_t m_items = 0;
             bool m_used = false;
+
+            ~Bucket() {
+                for (Entry *entry = m_head; entry != nullptr; entry = entry->m_next) {
+                    delete entry;
+                }
+            }
         };
 
         nk::Vector<Bucket> m_buckets{};
@@ -40,10 +50,18 @@ namespace nk {
         }
 
         void insert(const K &key, const V &value) {
+            grow();
+
             auto bucket_idx = hash(key) % m_buckets.length();
             auto &bucket = m_buckets[bucket_idx];
-            bucket.m_used = true;
-            bucket.m_entries.push(Entry{key, value});
+            auto entry = new Entry({key, value});
+
+            if (bucket.m_tail != nullptr)
+                bucket.m_tail->m_next = entry;
+            bucket.m_tail = entry;
+            if (bucket.m_head == nullptr)
+                bucket.m_head = entry;
+            bucket.m_items++;
         }
 
         Optional<V> find(const K &key) {
@@ -53,9 +71,10 @@ namespace nk {
             if (!bucket.m_used)
                 return Optional<V>();
 
-            for (const Entry &entry : bucket.m_entries)
-                if (entry.m_key == key)
-                    return entry.m_value;
+            for (Entry *entry = bucket.m_head; entry->m_next != nullptr; entry = entry->m_next) {
+                if (entry->m_key == key)
+                    return entry->m_value;
+            }
 
             return Optional<V>();
         }
@@ -63,13 +82,45 @@ namespace nk {
         size_t length() {
             size_t result = 0;
             for (const Bucket &buck : m_buckets)
-                result += buck.m_entries.length();
+                result += buck.m_items;
             return result;
         }
 
-    private:
-        void rehash() {
+        float load_factor() {
+            return (float) length() / (float) m_buckets.length();
+        }
 
+    private:
+        void grow() {
+            if (load_factor() <= max_load_factor)
+                return;
+
+            // Create new buckets
+            for (size_t i = 0; i < m_buckets.length(); i++) {
+                m_buckets.push(Bucket{});
+            }
+
+            // Rehash
+            for (const Bucket &bucket : m_buckets) {
+                for (Entry *entry = bucket.m_head; entry->m_next != nullptr; entry = entry->m_next) {
+                    // Recalculate hash
+                    auto new_bucket_idx = hash(entry->m_key) % m_buckets.length();
+                    Bucket &new_bucket = m_buckets[new_bucket_idx];
+
+                    // Remove from old bucket
+                    entry->m_prev->m_next = entry->m_next;
+
+                    // Insert into new bucket
+                    entry->m_prev = new_bucket.m_tail;
+                    entry->m_next = nullptr;
+
+                    if (new_bucket.m_tail != nullptr)
+                        new_bucket.m_tail->m_next = entry;
+                    new_bucket.m_tail = entry;
+                    if (new_bucket.m_head == nullptr)
+                        new_bucket.m_head = entry;
+                }
+            }
         }
 
 
