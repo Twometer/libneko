c2c9da5f9716ef302c5ea5e2015a1b1cbfb08c22
Twometer <twometer@outlook.de>
2022-06-05 15:35:31 +0200

    Fix bugs in ByteBuffer

--- src/ByteBuffer.hpp
+++ src/ByteBuffer.hpp
@@ -9,6 +9,7 @@
 #include <stddef.h>
 #include <string.h>
 #include <assert.h>
+#include "Types.hpp"
 
 namespace nk {
 
@@ -41,6 +42,8 @@ namespace nk {
 
         void move_position(long offset);
 
+        u8 *data() { return m_buffer; };
+
         template<typename T>
         T read(size_t index) const {
             assert(index < m_size);
@@ -53,20 +56,23 @@ namespace nk {
 
         template<typename T>
         T read() {
-            return read<T>(m_position += sizeof(T));
+            auto data = read<T>(m_position);
+            m_position += sizeof(T);
+            return data;
         }
 
         template<typename T>
         void write(size_t index, const T &data) const {
             assert(index < m_size);
-            assert(index + sizeof(T) < m_size);
+            assert(index + sizeof(T) <= m_size);
 
             memcpy(m_buffer + index, &data, sizeof(T));
         }
 
         template<typename T>
         void write(const T &data) {
-            write<T>(m_position += sizeof(T), data);
+            write<T>(m_position, data);
+            m_position += sizeof(T);
         }
     };
 
